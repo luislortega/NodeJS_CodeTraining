51d6c2fc6f65ddb99c08db994268052dfb22c31b
Luis Leon <leon.luisgerardo775@gmail.com>
2018-11-19 04:25:48 -0600

    - Cors, Morgan and Body-parser configured

--- VUE_NODE_JS_1_RELOAD/server/src/app.js
+++ VUE_NODE_JS_1_RELOAD/server/src/app.js
@@ -1,1 +1,18 @@
-console.log('hi')
+// Imports
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express(); const port = 8081
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('IM READY.')
+})
+
+app.listen(port, () => {
+  console.log(`BACKEND RUNNIG IN PORT ${port}`)
+})
