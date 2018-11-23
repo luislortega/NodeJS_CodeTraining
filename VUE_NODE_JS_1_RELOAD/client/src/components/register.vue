f17b9f69e3268916ed298c77813e293bfcdb6d10
Luis Leon <leon.luisgerardo775@gmail.com>
2018-11-23 02:04:59 -0600

    - Show the errors (400) in front end

--- VUE_NODE_JS_1_RELOAD/client/src/components/register.vue
+++ VUE_NODE_JS_1_RELOAD/client/src/components/register.vue
@@ -3,41 +3,48 @@
     <h1>REGISTER</h1>
     <input type="email" name="email" placeholder="email" v-model="email"/>
     <input type="password" name="password" placeholder="password" v-model="password"/>
+    <br/>
+      <div class="error" v-html="error"/>
+    <br/>
     <button @click="register">Send</button>
   </div>
 </template>
 
 <script>
-import AuthService from '@/services/AuthService'
-
+import AuthService from "@/services/AuthService";
 export default {
-  name: 'register',
-  data () {
+  data() {
     return {
-      email:'',
-      password:''
+      email: "",
+      password: "",
+      error: null
+    };
+  },
+  watch: {
+    email(value) {
+      console.log("email changed");
     }
   },
-  watch:{
-      email (value){
-          console.log("email changed")
-      }
-  },
-  methods:{
-      async register(){
+  methods: {
+    async register() {
+      try {
         const response = await AuthService.register({
-            email: this.email,
-            password: this.password
-        })
-        console.log(response)
+          email: this.email,
+          password: this.password
+        });
+        console.log(response);
+      } catch (error) {
+        this.error = error.response.data.error;
       }
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -50,5 +57,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.error {
+  color: red;
 }
 </style>
