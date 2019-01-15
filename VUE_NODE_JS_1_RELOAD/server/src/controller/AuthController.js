d69b0e09e6cbd09b02cea404fd3b68212fe1fb7e
Luis Leon <leon.luisgerardo775@gmail.com>
2019-01-15 14:28:35 -0600

    - bcrypt nodejs bugg fixed

--- VUE_NODE_JS_1_RELOAD/server/src/controller/AuthController.js
+++ VUE_NODE_JS_1_RELOAD/server/src/controller/AuthController.js
@@ -1,11 +1,8 @@
 const { User } = require('../models')
-
 const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 
-// Login method and token for a week
-function jwtSignUp (user) {
-  // Seconds... Minutes... Hours... Days...
+function jwtSignUser (user) {
   const ONE_WEEK = 60 * 60 * 24 * 7
   return jwt.sign(user, config.authentication.jwtSecret, {
     expiresIn: ONE_WEEK
@@ -13,21 +10,20 @@ function jwtSignUp (user) {
 }
 
 module.exports = {
-  // method send
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
-      // res.send({
-      //     message: `user ${req.body.email}`
-      // })
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use.'
       })
     }
   },
-  // method to login
   async login (req, res) {
     try {
       const { email, password } = req.body
@@ -40,30 +36,27 @@ module.exports = {
 
       if (!user) {
         return res.status(403).send({
-          error: "The user don't exist :(0"
+          error: 'this user dont exister was incorrect'
         })
       }
 
       const isPasswordValid = await user.comparePassword(password)
 
       if (!isPasswordValid) {
-        return res.status(400).send({
-          error: 'The password was incorrect'
+        return res.status(403).send({
+          error: 'The password isnt the correct'
         })
       }
 
       const userJson = user.toJSON()
 
-      /**
-       * Send the user and the token :p
-       */
       res.send({
         user: userJson,
-        token: jwtSignUp(userJson)
+        token: jwtSignUser(userJson)
       })
     } catch (err) {
       res.status(500).send({
-        error: 'And error has ocurred trying log in'
+        error: 'An error has occured trying to log in'
       })
     }
   }
--- VUE_NODE_JS_1_RELOAD/server/src/models/User.js
+++ VUE_NODE_JS_1_RELOAD/server/src/models/User.js
@@ -1,23 +1,21 @@
 const Promise = require('bluebird')
 const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
 
-// Haash for the password
 function hashPassword (user, options) {
   const SALT_FACTOR = 8
-  console.log('hash password')
+
   if (!user.changed('password')) {
     return
   }
 
   return bcrypt
     .genSaltAsync(SALT_FACTOR)
-    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(salt => bcrypt.hashSync(user.password, salt, null))
     .then(hash => {
       user.setDataValue('password', hash)
     })
 }
 
-// Need return the user model
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
@@ -25,17 +23,15 @@ module.exports = (sequelize, DataTypes) => {
       unique: true
     },
     password: DataTypes.STRING
-  },
-  {
+  }, {
     hooks: {
-      beforeCreate: hashPassword,
-      beforeUpdate: hashPassword,
-      beforeSave: hashPassword
+      beforeSave: hashPassword // Bugg fixed :p dont put the before update and create
     }
   })
 
   User.prototype.comparePassword = function (password) {
-    return bcrypt.compareAsync(password, this.password)
+    console.log('pass: ', password, 'pass: ', this.password)
+    return bcrypt.compareSync(password, this.password)
   }
 
   return User
