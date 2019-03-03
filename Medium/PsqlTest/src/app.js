2852def46681fdd2c264e73d8475a3c0a1506181
Luis Leon <leon.luisgerardo775@gmail.com>
2019-03-03 03:45:05 -0600

    - Routes with postgres working. :)

---
+++ Medium/PsqlTest/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import models from './models';
+
+const app = express();
+
+require('./config/routes')(app) //routes
+
+models.sequelize.sync({ force: true }).then(() => {
+  app.listen(8081, () => {
+    console.log('👽 Backend running');
+  });
+});
